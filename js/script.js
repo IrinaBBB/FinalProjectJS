42d3816ea972e8ee953d02bed28cc6021ef9e1a8
Irina Balkarova <irb023@post.uit.no>
2018-12-12 20:04:50 +0100

    v1.0

---
+++ js/script.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+  'use strict';
+  /* 
+    Header -> Pop-up window 
+  */
+
+  let headerBtn = document.querySelector('.header_btn'),
+    popupEn = document.querySelector('.popup_engineer'),
+    overlay = document.querySelector('.overlay'),
+    close = document.querySelector('.popup_close');
+
+
+  headerBtn.addEventListener('click', function () {
+    popupEn.style.display = 'block';
+    overlay.style.display = 'block';
+  });
+
+  function closeModal() {
+    popupEn.style.display = 'none';
+    overlay.style.display = 'none';
+  }
+
+  overlay.addEventListener('click', closeModal);
+  close.addEventListener('click', closeModal);
+
+});
