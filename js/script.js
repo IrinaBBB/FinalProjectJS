346757d49afff6582db3e0161ed2ab78f56f8a09
Irina Balkarova <irb023@post.uit.no>
2018-12-13 12:39:43 +0100

    v2.0

--- js/script.js
+++ js/script.js
@@ -8,20 +8,27 @@ window.addEventListener('DOMContentLoaded', function () {
   let headerBtn = document.querySelector('.header_btn'),
     popupEn = document.querySelector('.popup_engineer'),
     overlay = document.querySelector('.overlay'),
-    close = document.getElementsByClassName('popup_close')[1];
+    close = document.getElementsByClassName('popup_close')[1],
+    phoneLink = document.getElementsByClassName('phone_link');
 
-
-  headerBtn.addEventListener('click', function () {
+  function openModal() {
     popupEn.style.display = 'block';
     overlay.style.display = 'block';
-  });
+  }
 
   function closeModal() {
     popupEn.style.display = 'none';
     overlay.style.display = 'none';
   }
-  close.addEventListener('click', closeModal);
 
+
+  headerBtn.addEventListener('click', openModal);
+
+  for (let i = 0; i < phoneLink.length; i++) {
+    phoneLink[i].addEventListener('click', openModal);
+  }
+
+  close.addEventListener('click', closeModal);
   popupEn.addEventListener('click', function (event) {
 
     if (event.target.closest(".popup_dialog")) return;
