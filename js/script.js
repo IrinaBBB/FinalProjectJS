d45c6479d987c974796f69bdd9bd2fbb1aea59bd
Irina Balkarova <irb023@post.uit.no>
2018-12-13 13:29:14 +0100

    v2.1

--- js/script.js
+++ js/script.js
@@ -2,37 +2,63 @@ window.addEventListener('DOMContentLoaded', function () {
 
   'use strict';
   /* 
-    Header -> Pop-up window
+    Pop-up windows
   */
 
   let headerBtn = document.querySelector('.header_btn'),
     popupEn = document.querySelector('.popup_engineer'),
+    popup = document.querySelector('.popup'),
     overlay = document.querySelector('.overlay'),
-    close = document.getElementsByClassName('popup_close')[1],
+    close = document.getElementsByClassName('popup_close')[0],
+    closeEn = document.getElementsByClassName('popup_close')[1],
     phoneLink = document.getElementsByClassName('phone_link');
 
-  function openModal() {
+    console.log(close);
+
+  // Delayed Modal Popup
+
+  window.setTimeout(function () {
+    popup.style.display = 'block';
+    overlay.style.display = 'block';
+  }, 5000);
+
+  function closeModal() {
+    popup.style.display = 'none';
+    overlay.style.display = 'none';
+  }
+
+  close.addEventListener('click', closeModal);
+  popup.addEventListener('click', function (event) {
+
+    if (event.target.closest(".popup-dialog")) return;
+    closeModal();
+
+  });
+
+  // Engineer Popup
+
+  function openEnModal() {
     popupEn.style.display = 'block';
     overlay.style.display = 'block';
   }
 
-  function closeModal() {
+  function closeEnModal() {
     popupEn.style.display = 'none';
     overlay.style.display = 'none';
   }
 
 
-  headerBtn.addEventListener('click', openModal);
+  headerBtn.addEventListener('click', openEnModal);
 
   for (let i = 0; i < phoneLink.length; i++) {
-    phoneLink[i].addEventListener('click', openModal);
+    phoneLink[i].addEventListener('click', openEnModal);
   }
 
-  close.addEventListener('click', closeModal);
+  closeEn.addEventListener('click', closeEnModal);
   popupEn.addEventListener('click', function (event) {
 
     if (event.target.closest(".popup_dialog")) return;
-    closeModal();
+    closeEnModal();
 
   });
 
