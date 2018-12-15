7474380891fb7547e8e22d26067fdc99c9e1c9d3
Irina Balkarova <irb023@post.uit.no>
2018-12-15 18:45:57 +0100

    v4.1

--- js/script.js
+++ js/script.js
@@ -241,12 +241,90 @@ window.addEventListener('DOMContentLoaded', function () {
 
     }
 
-    
+
     for (let i = 0; i < imgLink.length; i++) {
         img.addEventListener('click', closeImage);
     }
 
     overlay2.addEventListener('click', closeImage);
 
-});
 
+
+    /* 
+            Forms
+    */
+
+   let message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся!',
+    failure: 'Что-то пошло не так...'
+};
+
+    let phoneInput = document.querySelectorAll('.user_phone'),
+        form = document.getElementsByClassName('main_form'),
+        input = document.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+    console.log(input);
+
+    for (let i = 0; i < phoneInput.length; i++) {
+        phoneInput[i].addEventListener('input', function () {
+            this.value = this.value.replace(/[^0-9+]/ig, '');
+        });
+    }
+
+  
+
+    function sendForm(elem) {
+        elem.addEventListener('submit', function (e) {
+            e.preventDefault();
+            elem.appendChild(statusMessage);
+            let formData = new FormData(elem);
+
+
+            function postData(data) {
+                return new Promise(function (resolve, reject) {
+                    let request = new XMLHttpRequest();
+
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+                    request.onreadystatechange = function () {
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState === 4) {
+                            if (request.status == 200 && request.status < 3) {
+                                resolve();
+                            } else {
+                                reject();
+                            }
+                        }
+                    }
+
+                    request.send(data);
+
+                });
+            }
+
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
+            }
+
+            postData(formData)
+                .then(() => statusMessage.innerHTML = message.loading)
+                .then(() => statusMessage.innerHTML = message.success)
+                .catch(() => statusMessage.innerHTML = message.failure)
+                .then(clearInput);
+
+
+        });
+    }
+
+    for (let i = 0; i < form.length; i++) {
+        sendForm(form[i]);
+    }
+    
+    
+
+
+});
