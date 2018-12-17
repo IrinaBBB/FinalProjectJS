55d0ece025b85966f008b4fed1ca701cda1f4787
Irina Balkarova <irb023@post.uit.no>
2018-12-18 00:41:57 +0100

    v6.2

---
+++ src/parts/popups.js
@@ -0,0 +1,66 @@
+ /* 
+      Pop-up windows
+  */
+
+function popUps() {
+ let headerBtn = document.querySelector('.header_btn'),
+   popupEn = document.querySelector('.popup_engineer'),
+   popup = document.querySelector('.popup'),
+   overlay = document.querySelector('.overlay'),
+   close = document.getElementsByClassName('popup_close')[0],
+   closeEn = document.getElementsByClassName('popup_close')[1],
+   phoneLink = document.getElementsByClassName('phone_link');
+
+
+
+
+ // Delayed Modal Popup
+
+ window.setTimeout(function () {
+   popup.style.display = 'block';
+   overlay.style.display = 'block';
+ }, 60000);
+
+ function closeModal() {
+   popup.style.display = 'none';
+   overlay.style.display = 'none';
+ }
+
+ close.addEventListener('click', closeModal);
+ popup.addEventListener('click', function (event) {
+
+   if (event.target.closest(".popup-calc")) return;
+   closeModal();
+
+ });
+
+ // Engineer Popup
+
+ function openEnModal() {
+   popupEn.style.display = 'block';
+   overlay.style.display = 'block';
+ }
+
+ function closeEnModal() {
+   popupEn.style.display = 'none';
+   overlay.style.display = 'none';
+ }
+
+
+ headerBtn.addEventListener('click', openEnModal);
+
+ for (let i = 0; i < phoneLink.length; i++) {
+   phoneLink[i].addEventListener('click', openEnModal);
+ }
+
+ closeEn.addEventListener('click', closeEnModal);
+ popupEn.addEventListener('click', function (event) {
+
+   if (event.target.closest(".popup_dialog")) return;
+   closeEnModal();
+
+ });
+
+}
+
+module.exports = popUps;
